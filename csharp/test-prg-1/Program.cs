7c35fa9be6d9c026488e9afb932aac8c6c51d2ce
Asher <carneiroasher@gmail.com>
2025-05-26 22:09:08 +0530

    basic csharp prg hello world test prg 1

---
+++ csharp/test-prg-1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Diagnostics.Contracts;
+
+namespace stupid
+{
+    internal class Balls
+    {
+        static void Main(string[] jesse)
+        {
+            Console.WriteLine($"Balls Man, {jesse[0]}");
+        }
+    }
+}
