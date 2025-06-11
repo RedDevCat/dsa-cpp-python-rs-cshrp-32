3019ba79f32cc442316e4f79de21b9c851d46ebb
Asher <carneiroasher@gmail.com>
2025-06-11 16:44:58 +0530

    linear, binary search, and both in one with input added

--- csharp/array-linear-search/Program.cs
+++ csharp/array-linear-search/Program.cs
@@ -1,7 +1,48 @@
-﻿string[] abcde = new string[] { "asher", "ballman", "comcast", "daedra", "estrogen" };
-
-int[] myarr = new int[5] { 1, 2, 3, 4, 5 };
+﻿//string[] abcde = new string[] { "asher", "ballman", "comcast", "daedra", "estrogen" };
 
 //this is called bruteforce searching method
 
+//for each version cleaner, no access to indexes
+// foreach (int i in myarr)
+// {
+//     if (i == targetValue)
+//     {
+//         Console.WriteLine("Found at:" + i);
+//         found = true;
+//         break;
+//     }
+// }
+// if (!found) // means if found bool is still false after this whole ass loop
+// {
+//     Console.WriteLine("Not Found");
+// }
 
+//for loop version
+
+using System;
+
+class Program
+{
+    public static void LinearSearch(int[] myarr, int targetValue)
+    {
+        for (int i = 0; i < myarr.Length; i++)
+        {
+            if (myarr[i] == targetValue)
+            {
+                Console.WriteLine("Linear Search");
+                Console.WriteLine(targetValue + " Found at index " + i);
+                return;
+            }
+        }
+        Console.WriteLine("Not Found");
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] myarr = new int[5] { 10, 20, 30, 40, 50 };
+        int targetValue = 30;
+        LinearSearch(myarr, targetValue);
+    }
+}
+
+// visualization: https://www.cs.usfca.edu/~galles/visualization/Search.html
---
+++ csharp/binarysearch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void BinarySearch(int[] myarr, int targetValue)
+    {
+        int low = 0, high = myarr.Length - 1;
+        int middle = 0;
+
+        while (low <= high)
+        {
+            middle = (low + high) / 2;
+
+            if (myarr[middle] == targetValue)
+            {   
+                Console.WriteLine("Binary Search");
+                Console.WriteLine("Value found at index: " + middle);
+                return;
+            }
+            else if (myarr[middle] < targetValue)
+            {
+                low = middle + 1;
+            }
+            else
+            {
+                high = middle - 1;
+            }
+        }
+
+        Console.WriteLine("Target not in array");
+        Console.ReadLine();
+    }
+
+    static void Main(string[] args)
+    {
+        int[] myarr = {1, 3, 5, 7, 9, 11, 13};
+        int target = 7;
+
+        BinarySearch(myarr, target);
+    }
+}
+
+//visualization: https://www.cs.usfca.edu/~galles/visualization/Search.html
